094b918b9b42e71cf200ac14a3bdc2c18510df0f
Abhishek kaushal <abhishekkaush39@gmail.com>
2025-08-01 15:46:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { createRoot } from "react-dom/client";
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
 />;
 import App from "./App";
-
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
